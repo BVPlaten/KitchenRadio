cd3f80f735d981ecd6c2b0683190fc4f7fff2e34
BVPlaten <bernd.von.platen@gmail.com>
2021-11-05 15:28:04 +0100

    Projektdateien hinzufügen.

---
+++ RadioServer/Data/IdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadioServer.Data
+{
+    public class IdentityDbContext : Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
